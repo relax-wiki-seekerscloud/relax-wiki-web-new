f3311f7c71ea9c5e535f59dd380671dbada89080
giharapathi <88247382+giharapathi@users.noreply.github.com>
2023-06-17 23:01:08 +0530

    landing page

--- src/app/modules/share/share-routing.module.ts
+++ src/app/modules/share/share-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShareComponent } from './share.component';
 import {FeedbackFormComponent} from "./components/feedback-form/feedback-form.component";
+import {ConsoleDefaultComponent} from "../console/components/console-default/console-default.component";
 
-const routes: Routes = [{ path: '', component: ShareComponent },
+const routes: Routes = [
+  { path: '', component: ShareComponent },
+  {path: 'console-default',component:ConsoleDefaultComponent},
   {path:'feedback-form', component: FeedbackFormComponent},
 ];
 
